14a106954861d7d8f941b313b5fdc86d839d4a1d
Antoine Tenart <atenart@redhat.com>
2025-07-15 18:13:02 +0200

    pcap: insert VLAN accel data into the payload
    When using AF_PACKET utilities, if the VLAN information is hw accelerated it is
    inserted in the kernel before the packet reaches userspace. Because of this,
    those tools display VLAN data as part of the packet even when accelerated.
    
    This is not the case in Retis, as we report the packet as-is and the VLAN hw
    acceleration data as part of the event. When using Retis, both cases work well
    as the VLAN data is printed one way or another (in the packet or as an event
    section). But when converting the packet to the PCAP format, the result is hw
    acceleration data is not inserted and the result differs from what we can
    expected when using AF_PACKET utilities.
    
    Insert the VLAN tag when accelerated by default when converting events to PCAP
    data in Retis, to match the other utilities expectations. This can be disabled
    with a cli flag.
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- retis/src/process/cli/pcap.rs
+++ retis/src/process/cli/pcap.rs
@@ -178,7 +178,7 @@ impl EventParser {
     }
 
     /// Parse & process a single Retis event.
-    fn parse(&mut self, event: &mut Event) -> Result<Vec<Block<'_>>> {
+    fn parse(&mut self, event: &mut Event, fixups: bool) -> Result<Vec<Block<'_>>> {
         // Having a common & a kernel section is mandatory for now, seeing a
         // filtered event w/o one of those is bogus.
         let common = event
@@ -213,7 +213,26 @@ impl EventParser {
 
         let id = self.process_interface(event, packet.kind, &mut v)?;
 
-        // Add the packet itself.
+        // Add the packet itself. If VLAN acceleration information is there,
+        // insert it in the payload.
+        let mut data = packet.data.0.clone();
+        let mut len = packet.len;
+        match &event.skb {
+            Some(skb) if fixups && skb.vlan_accel.is_some() => {
+                // Unwrap as we just made sure the section is there.
+                let vlan_accel = skb.vlan_accel.as_ref().unwrap();
+                let tag = (vlan_accel.proto as u32) << 16
+                    | ((vlan_accel.pcp & 0x7) as u32) << 13
+                    | (vlan_accel.dei as u32) << 12
+                    | (vlan_accel.vid & 0xfff) as u32;
+
+                data.splice(12..12, tag.to_be_bytes());
+
+                len += 4;
+            }
+            _ => (),
+        }
+
         v.push(
             EnhancedPacketBlock {
                 interface_id: id,
@@ -221,8 +240,8 @@ impl EventParser {
                     TimeSpec::new(0, common.timestamp as i64)
                         + self.ts_off.clone().unwrap_or_default(),
                 ) as u64),
-                original_len: packet.len,
-                data: Cow::Borrowed(&packet.data.0),
+                original_len: len,
+                data: Cow::Owned(data),
                 options: vec![
                     EnhancedPacketOption::Common(CommonOption::Comment(Cow::Owned(comment))),
                     EnhancedPacketOption::Common(CommonOption::CustomUtf8Copiable(
@@ -257,6 +276,12 @@ impl EventParser {
 #[derive(Parser, Debug, Default)]
 #[command(name = "pcap", about = "Generate a PCAP file from stored events.")]
 pub(crate) struct Pcap {
+    #[arg(
+        long,
+        help = "Post-processing might be performed on events when generating a PCAP output. This disable such fixups to keep the PCAP output entirely in sync with the events from the input. List of fixups:
+- VLAN hardware acceleration: if available (the vlan_accel section of the skb event section is present), a VLAN header is inserted in the packet payload."
+    )]
+    pub(super) no_fixups: bool,
     #[arg(
         short,
         long,
@@ -313,6 +338,7 @@ impl SubCommandParserRunner for Pcap {
 
         handle_events(
             &self.input.clone().unwrap_or_default(),
+            !self.no_fixups,
             &filter,
             &mut EventParser::new(),
             write_block,
@@ -324,6 +350,7 @@ impl SubCommandParserRunner for Pcap {
 /// Internal logic to retrieve our events to feed the parser.
 fn handle_events<F>(
     input: &InputDataFile,
+    fixups: bool,
     filter: &dyn Fn(&str, &str) -> bool,
     parser: &mut EventParser,
     mut writer_callback: F,
@@ -350,7 +377,7 @@ where
                     matched = true;
 
                     // Parse the event and then write the pcap blocks to the file.
-                    let parsed_blocks = parser.parse(&mut event)?;
+                    let parsed_blocks = parser.parse(&mut event, fixups)?;
                     for b in parsed_blocks {
                         writer_callback(&b)?;
                     }
@@ -583,6 +610,7 @@ mod tests {
             };
             match handle_events(
                 &file_path.parse().expect("Could not parse input file"),
+                false,
                 &filter,
                 &mut EventParser::new(),
                 write_blocks,

5839169b78049f0b25b7ff6960c472480bba9792
Antoine Tenart <atenart@redhat.com>
2026-07-29 16:48:30 +0200

    tracking: skb: do not add tracking info for hook-less probes
    Avoid adding new tracking entries if we are not generating events.
    
    Signed-off-by: Antoine Tenart <atenart@redhat.com>

--- retis/src/core/probe/kernel/bpf/include/common.h
+++ retis/src/core/probe/kernel/bpf/include/common.h
@@ -109,9 +109,6 @@ struct {
 #define DEFINE_HOOK_RAW(statements)					\
 	__DEFINE_NAMED_HOOK(__PROG_NAME, true, statements)
 
-/* Number of hooks installed, used to micro-optimize the call chain */
-const volatile u32 nhooks = 0;
-
 /* Hook definition, aimed at being replaced before the program is attached. The
  * temporary retval is volatile to not let the compiler think he can optimize
  * it. Credits to the XDP dispatcher.
